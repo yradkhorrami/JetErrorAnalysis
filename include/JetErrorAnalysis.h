a705b61e262ed018868fe366a405d95e8fac3f73
Yasser Radkhorrami <yasser.radkhorrami@desy.de>
2021-04-26 20:17:57 +0200

    get processor running with TrueJet_Parser included

--- include/JetErrorAnalysis.h
+++ include/JetErrorAnalysis.h
@@ -1,2 +1,111 @@
-#ifndef JetErrorParametrization_h
-#define JetErrorParametrization_h 1
+#ifndef JetErrorAnalysis_h
+#define JetErrorAnalysis_h 1
+
+//#include "TrueJet.h"
+#include "marlin/Processor.h"
+#include "lcio.h"
+#include <EVENT/LCCollection.h>
+#include <EVENT/MCParticle.h>
+#include <EVENT/ReconstructedParticle.h>
+#include <EVENT/LCRelation.h>
+#include <UTIL/LCRelationNavigator.h>
+#include "IMPL/LCCollectionVec.h"
+#include <IMPL/ReconstructedParticleImpl.h>
+#include <IMPL/ParticleIDImpl.h>
+#include <string>
+#include "TrueJet_Parser.h"
+#include <string>
+#include "TLorentzVector.h"
+#include <TFile.h>
+#include <TTree.h>
+class TFile;
+class TH1F;
+class TH1I;
+class TH2I;
+class TH2F;
+class TTree;
+
+using namespace lcio ;
+using namespace marlin ;
+
+class JetErrorAnalysis : public Processor , public TrueJet_Parser
+{
+
+	public:
+
+		virtual Processor*  newProcessor()
+		{
+			return new JetErrorAnalysis;
+		}
+
+		JetErrorAnalysis();
+		virtual ~JetErrorAnalysis() = default;
+		JetErrorAnalysis(const JetErrorAnalysis&) = delete;
+		JetErrorAnalysis& operator=(const JetErrorAnalysis&) = delete;
+
+		/*
+		* Called at the begin of the job before anything is read.
+		* Use to initialize the processor, e.g. book histograms.
+		*/
+		virtual void init();
+
+		/*
+		* Called for every run.
+		*/
+		virtual void processRunHeader();
+
+		/*
+		* Called for every event - the working horse.
+		*/
+		virtual void processEvent( LCEvent * evt );
+
+
+		virtual void check();
+
+		/*
+		* Called after data processing for clean up.
+		*/
+		virtual void end();
+
+		std::string get_recoMCTruthLink()
+		{
+			return _recoMCTruthLink;
+		};
+
+	protected:
+
+		/*
+		* Input collection name.
+		*/
+		typedef	std::vector<int>		IntVector;
+		typedef	std::vector<float>		floatVector;
+
+		int					m_nRun;
+		int					m_nEvt;
+		int					m_nRunSum;
+		int					m_nEvtSum;
+		int					m_nTrueJets;
+		int					m_nTrueLeptons;
+		int					m_nRecoJets;
+		int					m_nRecoLeptons;
+		int					m_HDecayMode;
+		int					m_nSLDecayBHadron;
+		int					m_nSLDecayCHadron;
+		int					m_nSLDecayTotal;
+		IntVector				m_trueJetType{};
+
+	private:
+
+
+		std::string				_MCParticleColllectionName{};
+		std::string				_recoParticleCollectionName{};
+		std::string				_recoMCTruthLink{};
+//		std::string				_trueJetCollectionName{};
+		std::string				_recoJetCollectionName{};
+		std::string				m_outputFile{};
+		TFile					*m_pTFile;
+	        TTree					*m_pTTree;
+
+};
+
+#endif
--- src/JetErrorAnalysis.cc
+++ src/JetErrorAnalysis.cc
@@ -3,3 +3,226 @@
 #include <cmath>
 #include <iostream>
 #include <iomanip>
+
+
+// ----- include for verbosity dependend logging ---------
+#include "marlin/VerbosityLevels.h"
+
+#ifdef MARLIN_USE_AIDA
+#include <marlin/AIDAProcessor.h>
+#include <AIDA/IHistogramFactory.h>
+#include <AIDA/ICloud1D.h>
+//#include <AIDA/IHistogram1D.h>
+#endif // MARLIN_USE_AIDA
+
+
+using namespace lcio ;
+using namespace marlin ;
+using namespace std ;
+
+JetErrorAnalysis aJetErrorAnalysis ;
+
+JetErrorAnalysis::JetErrorAnalysis() : Processor("JetErrorAnalysis"),
+m_nRun(0),
+m_nEvt(0),
+m_nRunSum(0),
+m_nEvtSum(0),
+m_nTrueJets(0),
+m_nTrueLeptons(0),
+m_nRecoJets(0),
+m_nRecoLeptons(0),
+m_HDecayMode(0),
+m_nSLDecayBHadron(0),
+m_nSLDecayCHadron(0),
+m_nSLDecayTotal(0),
+m_pTFile(NULL),
+m_pTTree(NULL)
+{
+
+	// modify processor description
+	_description = "JetErrorAnalysis does whatever it does ..." ;
+
+
+	// register steering parameters: name, description, class-variable, default value
+
+
+	// Inputs: MC-particles, Reco-particles, the link between the two
+
+	registerInputCollection( LCIO::MCPARTICLE,
+				"MCParticleCollection" ,
+				"Name of the MCParticle collection"  ,
+				_MCParticleColllectionName ,
+				std::string("MCParticlesSkimmed")
+				);
+
+	registerInputCollection( LCIO::RECONSTRUCTEDPARTICLE,
+				"RecoParticleCollection" ,
+				"Name of the ReconstructedParticles input collection"  ,
+				_recoParticleCollectionName ,
+				std::string("PandoraPFOs")
+				);
+
+	registerInputCollection( LCIO::LCRELATION,
+				"RecoMCTruthLink",
+				"Name of the RecoMCTruthLink input collection"  ,
+				_recoMCTruthLink,
+				std::string("RecoMCTruthLink")
+				);
+
+	registerProcessorParameter("outputFilename",
+				"name of output file",
+				m_outputFile,
+				std::string("")
+				);
+
+
+	// Inputs: True jets (as a recoparticle, will be the sum of the _reconstructed particles_
+	// created by the true particles in each true jet, in the RecoMCTruthLink sense.
+	// link jet-to-reco particles, link jet-to-MC-particles.
+
+	registerInputCollection( LCIO::RECONSTRUCTEDPARTICLE,
+				"TrueJets" ,
+				"Name of the TrueJetCollection input collection",
+				_trueJetCollectionName ,
+				std::string("TrueJets") ) ;
+
+	registerInputCollection( LCIO::RECONSTRUCTEDPARTICLE,
+				"FinalColourNeutrals" ,
+				"Name of the FinalColourNeutralCollection input collection"  ,
+				_finalColourNeutralCollectionName ,
+				std::string("FinalColourNeutrals") ) ;
+
+	registerInputCollection( LCIO::RECONSTRUCTEDPARTICLE,
+				"InitialColourNeutrals" ,
+				"Name of the InitialColourNeutralCollection input collection"  ,
+				_initialColourNeutralCollectionName ,
+				std::string("InitialColourNeutrals") ) ;
+
+
+	registerInputCollection( LCIO::LCRELATION,
+				"TrueJetPFOLink" ,
+				"Name of the TrueJetPFOLink input collection"  ,
+				_trueJetPFOLink,
+				std::string("TrueJetPFOLink") ) ;
+
+	registerInputCollection( LCIO::LCRELATION,
+				"TrueJetMCParticleLink" ,
+				"Name of the TrueJetMCParticleLink input collection"  ,
+				_trueJetMCParticleLink,
+				std::string("TrueJetMCParticleLink") ) ;
+
+	registerInputCollection( LCIO::LCRELATION,
+				"FinalElementonLink rueJetMCParticleLink" ,
+				"Name of the  FinalElementonLink input collection"	,
+				_finalElementonLink,
+				std::string("FinalElementonLink") ) ;
+
+	registerInputCollection( LCIO::LCRELATION,
+				"InitialElementonLink" ,
+				"Name of the  InitialElementonLink input collection"  ,
+				_initialElementonLink,
+				std::string("InitialElementonLink") ) ;
+
+	registerInputCollection( LCIO::LCRELATION,
+				"FinalColourNeutralLink" ,
+				"Name of the  FinalColourNeutralLink input collection"  ,
+				_finalColourNeutralLink,
+				std::string("FinalColourNeutralLink") ) ;
+
+	registerInputCollection( LCIO::LCRELATION,
+				"InitialColourNeutralLink" ,
+				"Name of the  InitialColourNeutralLink input collection"  ,
+				_initialColourNeutralLink,
+				std::string("InitialColourNeutralLink") ) ;
+
+}
+
+
+void JetErrorAnalysis::init()
+{
+
+	streamlog_out(DEBUG6) << "   init called  " << std::endl ;
+
+	// usually a good idea to
+	printParameters() ;
+
+	m_nRun = 0 ;
+	m_nEvt = 0 ;
+
+	m_pTFile = new TFile(m_outputFile.c_str(),"recreate");
+
+	m_pTTree = new TTree("eventTree","eventTree");
+	m_pTTree->SetDirectory(m_pTFile);
+	m_pTTree->Branch("run", &m_nRun, "run/I");
+	m_pTTree->Branch("event", &m_nEvt, "event/I");
+	m_pTTree->Branch("nTrueJets",&m_nTrueJets,"nTrueJets/I") ;
+	m_pTTree->Branch("nTrueLeptons",&m_nTrueLeptons,"nTrueLeptons/I") ;
+	m_pTTree->Branch("nRecoJets",&m_nRecoJets,"nRecoJets/I") ;
+	m_pTTree->Branch("nRecoLeptons",&m_nRecoLeptons,"nRecoLeptons/I") ;
+	m_pTTree->Branch("HDecayMode",&m_HDecayMode,"HDecayMode/I") ;
+	m_pTTree->Branch("nSLDecayBHadron",&m_nSLDecayBHadron,"nSLDecayBHadron/I") ;
+	m_pTTree->Branch("nSLDecayCHadron",&m_nSLDecayCHadron,"nSLDecayCHadron/I") ;
+	m_pTTree->Branch("nSLDecayTotal",&m_nSLDecayTotal,"nSLDecayTotal/I") ;
+	m_pTTree->Branch("trueJetType",&m_trueJetType) ;
+
+}
+
+
+void JetErrorAnalysis::processRunHeader()
+{
+    m_nRun++ ;
+}
+
+
+void JetErrorAnalysis::processEvent( LCEvent* pLCEvent)
+{
+	LCCollection *recoJetCol{};
+	LCCollection *trueJetCol{};
+	m_nRun = pLCEvent->getRunNumber();
+	m_nEvt = pLCEvent->getEventNumber();
+
+	try
+	{
+//		recoJetCol		= pLCEvent->getCollection( _recoJetCollectionName );
+		trueJetCol		= pLCEvent->getCollection( _trueJetCollectionName );
+		TrueJet_Parser* trueJet	= this;
+		trueJet->getall(pLCEvent);
+
+//		m_nRecoJets = recoJetCol->getNumberOfElements();
+
+		int njets = trueJet->njets();
+		for (int i_jet = 0 ; i_jet < njets ; i_jet++ )
+		{
+			m_trueJetType.push_back( type_jet( i_jet ) );
+			if ( type_jet( i_jet ) == 1 )
+			{
+				++m_nTrueJets;
+			}
+		}
+		m_nEvtSum++;
+		m_nEvt++ ;
+		m_pTTree->Fill();
+	}
+	catch(DataNotAvailableException &e)
+	{
+		streamlog_out(MESSAGE) << "JetErrorAnalysis : Input collections not found in event " << m_nEvt << std::endl;
+	}
+
+
+}
+
+
+void JetErrorAnalysis::check()
+{
+
+}
+
+void JetErrorAnalysis::end()
+{
+	m_pTFile->cd();
+	m_pTTree->Write();
+	m_pTFile->Close();
+	delete m_pTFile;
+
+
+}
